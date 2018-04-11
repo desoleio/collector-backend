f0c8f50caee7a19a04a0ab9936303960819900dc
Gojko Adzic <gojko@gojko.com>
2018-04-11 21:13:01 +0200

    pinpoint publisher works

--- app-repository/README.md
+++ app-repository/README.md
@@ -4,18 +4,9 @@
 
 ```
 npm i --no-optional
+find ./node_modules/ -exec touch '{}' \; 
 aws cloudformation package --template-file template.yaml --output-template-file output.yaml --s3-bucket desole-upload-1
 aws cloudformation deploy --template-file output.yaml --stack-name desole-3 --capabilities CAPABILITY_IAM --parameter-overrides CloudWatchNameSpace=DesoleEvents PinpointApplicationId=16ff1586be31415a927d1ee3e9ee0608
 ```
-
-## todo:
-
-* repackage so linting doesn't confuse cloudformation
-* add output to template.yaml so we have the endpoint
-* CORS handling
-* memory/timeout for lambda
-* s3 options
-  * encryption for storage
-  * expiry
 
 
--- pinpoint-publisher/src/lambda.js
+++ pinpoint-publisher/src/lambda.js
@@ -1,20 +1,17 @@
 'use strict';
 const AWS = require('aws-sdk'),
 	mobileAnalytics = new AWS.MobileAnalytics(),
+	pinpoint = new AWS.Pinpoint(),
 	parseSNSEvent = require('@desole/common/src/parse-sns-event'),
 	flat = require('flat'),
+	uaParser = require('useragent'),
 	MOBILE_HUB_APPLICATION = process.env.MOBILE_HUB_APPLICATION,
 	EVENT_NAME = process.env.EVENT_NAME,
+
 	storeSingleEvent = event => {
 		const clientContext = {
 				client: {
-					client_id: event.endpoint.id,
-					app_title: event.app.name,
-					app_version_name: event.app.stage,
-					app_version_code: event.app.version
-				},
-				env: {
-					platform: event.endpoint.platform
+					client_id: event.endpoint.id
 				},
 				services: {
 					mobile_analytics: {
@@ -25,14 +22,40 @@ const AWS = require('aws-sdk'),
 			analyticsEvent = {
 				eventType: EVENT_NAME,
 				timestamp: new Date(event.receivedAt).toISOString(),
-				attributes: flat(event)
-			};
+				attributes: flat(event, {delimiter: ' '})
+			},
+			userAgent = event.endpoint.userAgent && uaParser.parse(event.endpoint.userAgent);
 		delete analyticsEvent.attributes.timestamp;
 		delete analyticsEvent.attributes.receivedAt;
-		return mobileAnalytics.putEvents({
+		if (userAgent) {
+			analyticsEvent.attributes['browser type'] = userAgent.family;
+			analyticsEvent.attributes['browser version'] = userAgent.toVersion();
+			analyticsEvent.attributes['browser OS'] = userAgent.os.toString();
+		}
+		return pinpoint.updateEndpoint({
+			ApplicationId: MOBILE_HUB_APPLICATION,
+			EndpointId: event.endpoint.id,
+			EndpointRequest: {
+				Attributes: {
+					'deviceType': event.endpoint.deviceType
+				},
+				Demographic: {
+					AppVersion: event.app.version,
+					Locale: event.endpoint.language,
+					Platform: event.endpoint.platform,
+					Model: userAgent && userAgent.family,
+					ModelVersion: userAgent && userAgent.toVersion(),
+					PlatformVersion: userAgent && userAgent.os.toString()
+				},
+				Location: {
+					Country: event.endpoint.country
+				}
+			}
+		}).promise()
+		.then(() => mobileAnalytics.putEvents({
 			clientContext: JSON.stringify(clientContext),
 			events: [analyticsEvent]
-		}).promise();
+		}).promise());
 	};
 
 exports.handler = (event) => {
