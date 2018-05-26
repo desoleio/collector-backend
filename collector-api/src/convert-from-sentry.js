972b118ae601c42d9701ecb2bb369f9605bdcadb
simalexan <alexander.simovic@gmail.com>
2018-05-26 13:11:21 +0200

    adding tests and fixing uncaught Raven errors

--- collector-api/src/convert-from-sentry.js
+++ collector-api/src/convert-from-sentry.js
@@ -9,13 +9,13 @@ const createGenericLambdaEvent = require('./create-generic-lambda-event'),
 
 module.exports = function convertFromSentry(lambdaProxyEvent, lambdaContext) {
 	const body = JSON.parse(lambdaProxyEvent.body),
-		exception = body.exception.values[0],
+		exception = (body.exception && body.exception.values[0]) || body,
 		breadcrumbs = body.breadcrumbs,
 		desoleEvent = Object.assign(createGenericLambdaEvent(lambdaProxyEvent), {
 			severity: body.level || 'error',
 			stack: getRavenStacktraces(exception),
-			type: exception.type,
-			message: exception.value,
+			type: exception.type || 'RuntimeError',
+			message: exception.value || body.message,
 			timestamp: extractBreadcrumbTimeStamp(breadcrumbs),
 			resource: body.request.url,
 			tags: body.tags
---
+++ collector-api/tests/convert-from-sentry.test.js
@@ -0,0 +1,39 @@
+/* global test, describe, test, expect */
+
+const convertFromSentry = require('../src/convert-from-sentry'),
+	lambdaProxyEvent = require('./test-events/lambda-proxy-event'),
+	wrapEvent = (event) => {
+		const result = JSON.parse(JSON.stringify(lambdaProxyEvent));
+		result.body = JSON.stringify(event);
+		return result;
+	};
+
+describe('Convert from Sentry', () => {
+	'use strict';
+
+	describe('Error type', () => {
+		test('should pickup the error type if defined', () => {
+			const event = wrapEvent(require('./test-events/raven-exception'));
+			expect(convertFromSentry(event).type).toBe('TypeError');
+		});
+
+		test('should use runtime error type if undefined', () => {
+			const event = wrapEvent(require('./test-events/raven-manually-tracked'));
+			expect(convertFromSentry(event).type).toBe('RuntimeError');
+		});
+	});
+
+	describe('Error message', () => {
+		test('should pickup the message if exception value defined', () => {
+			const event = wrapEvent(require('./test-events/raven-exception'));
+			expect(convertFromSentry(event).message).toBe(`Cannot read property 'captureException' of undefined`);
+		});
+
+		test('should use body message if exception value undefined', () => {
+			const event = wrapEvent(require('./test-events/raven-manually-tracked'));
+			expect(convertFromSentry(event).message).toBe('error');
+		});
+	});
+});
+
+
